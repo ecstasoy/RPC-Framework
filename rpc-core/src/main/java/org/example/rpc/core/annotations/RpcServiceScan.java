e65b1e51aa7bc4d76a0ca0884bced66cc6f1cd9c
Kunhua Huang <huang.kunh@northeastern.edu>
2024-10-01 23:17:43 -0400

    added annotation class

---
+++ rpc-core/src/main/java/org/example/rpc/core/annotations/RpcServiceScan.java
@@ -0,0 +1,17 @@
+package org.example.rpc.core.annotations;
+
+import org.example.rpc.core.spring.RpcBeanRegistrar;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * Annotation for scanning RPC services.
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Import(RpcBeanRegistrar.class)
+@Documented
+public @interface RpcServiceScan {
+  String[] basePackages() default {};
+}
